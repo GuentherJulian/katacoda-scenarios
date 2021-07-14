b98322e5cdbb7f2136da61d97abe46db1be5721f
Julian Günther <julian.guenther@capgemini.com>
2021-07-14 12:35:15 +0200

    test

--- publishTutorials.sh
+++
@@ -1,8 +0,0 @@
-rm -r repo/*/
-cp -r build/output/katacoda/*/ repo/
-cd repo/
-git add -A
-git config user.email "devonfw"
-git config user.name "devonfw"
-git commit -m "Updated tutorials"
-git push
